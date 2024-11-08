8558470096a331aa78bb98bdfa988f0815e2cd15
Brian Aji Pamungkas <brianajiks123@gmail.com>
2024-11-09 05:04:39 +0700

    add register user api, unit test, and helper

---
+++ test/test.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../src/common/prisma.service";
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class TestService {
+    constructor(private prismaService: PrismaService) {}
+
+    async deleteUser() {
+        await this.prismaService.user.deleteMany({
+            where: {
+                username: "test1"
+            }
+        });
+    }
+
+    async createUser() {
+        await this.prismaService.user.create({
+            data: {
+                username: "test1",
+                name: "test1",
+                password: await bcrypt.hash("test1", 10)
+            }
+        });
+    }
+}
